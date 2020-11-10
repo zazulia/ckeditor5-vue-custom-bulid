8da467c47401153b8e1d519a4a5a52d29b38505c
Alexey Zubarev <zajet@yusto.ru>
2020-11-10 15:22:53 +0300

    edit simple uploader for build

--- src/ckeditor5-upload/src/adapters/simpleuploadadapter.js
+++ src/ckeditor5-upload/src/adapters/simpleuploadadapter.js
@@ -7,11 +7,11 @@
  * @module upload/adapters/simpleuploadadapter
  */
 
-/* globals XMLHttpRequest, FormData, console */
+/* globals XMLHttpRequest, FormData */
 
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
-import { attachLinkToDocumentation } from '@ckeditor/ckeditor5-utils/src/ckeditorerror';
+import { logWarning } from '@ckeditor/ckeditor5-utils/src/ckeditorerror';
 
 /**
  * The Simple upload adapter allows uploading images to an application running on your server using
@@ -39,49 +39,47 @@ import { attachLinkToDocumentation } from '@ckeditor/ckeditor5-utils/src/ckedito
  * @extends module:core/plugin~Plugin
  */
 export default class SimpleUploadAdapter extends Plugin {
-    /**
-     * @inheritDoc
-     */
-    static get requires() {
-        return [ FileRepository ];
-    }
+	/**
+	 * @inheritDoc
+	 */
+	static get requires() {
+		return [ FileRepository ];
+	}
 
-    /**
-     * @inheritDoc
-     */
-    static get pluginName() {
-        return 'SimpleUploadAdapter';
-    }
+	/**
+	 * @inheritDoc
+	 */
+	static get pluginName() {
+		return 'SimpleUploadAdapter';
+	}
 
-    /**
-     * @inheritDoc
-     */
-    init() {
-        const options = this.editor.config.get( 'simpleUpload' );
+	/**
+	 * @inheritDoc
+	 */
+	init() {
+		const options = this.editor.config.get( 'simpleUpload' );
 
-        if ( !options ) {
-            return;
-        }
+		if ( !options ) {
+			return;
+		}
 
-        if ( !options.uploadUrl ) {
-            /**
-             * The {@link module:upload/adapters/simpleuploadadapter~SimpleUploadConfig#uploadUrl `config.simpleUpload.uploadUrl`}
-             * configuration required by the {@link module:upload/adapters/simpleuploadadapter~SimpleUploadAdapter `SimpleUploadAdapter`}
-             * is missing. Make sure the correct URL is specified for the image upload to work properly.
-             *
-             * @error simple-upload-adapter-missing-uploadUrl
-             */
-            console.warn( attachLinkToDocumentation(
-                'simple-upload-adapter-missing-uploadUrl: Missing the "uploadUrl" property in the "simpleUpload" editor configuration.'
-            ) );
+		if ( !options.uploadUrl ) {
+			/**
+			 * The {@link module:upload/adapters/simpleuploadadapter~SimpleUploadConfig#uploadUrl `config.simpleUpload.uploadUrl`}
+			 * configuration required by the {@link module:upload/adapters/simpleuploadadapter~SimpleUploadAdapter `SimpleUploadAdapter`}
+			 * is missing. Make sure the correct URL is specified for the image upload to work properly.
+			 *
+			 * @error simple-upload-adapter-missing-uploadurl
+			 */
+			logWarning( 'simple-upload-adapter-missing-uploadurl' );
 
-            return;
-        }
+			return;
+		}
 
-        this.editor.plugins.get( FileRepository ).createUploadAdapter = loader => {
-            return new Adapter( loader, options );
-        };
-    }
+		this.editor.plugins.get( FileRepository ).createUploadAdapter = loader => {
+			return new Adapter( loader, options );
+		};
+	}
 }
 
 /**
@@ -91,128 +89,133 @@ export default class SimpleUploadAdapter extends Plugin {
  * @implements module:upload/filerepository~UploadAdapter
  */
 class Adapter {
-    /**
-     * Creates a new adapter instance.
-     *
-     * @param {module:upload/filerepository~FileLoader} loader
-     * @param {module:upload/adapters/simpleuploadadapter~SimpleUploadConfig} options
-     */
-    constructor( loader, options ) {
-        /**
-         * FileLoader instance to use during the upload.
-         *
-         * @member {module:upload/filerepository~FileLoader} #loader
-         */
-        this.loader = loader;
+	/**
+	 * Creates a new adapter instance.
+	 *
+	 * @param {module:upload/filerepository~FileLoader} loader
+	 * @param {module:upload/adapters/simpleuploadadapter~SimpleUploadConfig} options
+	 */
+	constructor( loader, options ) {
+		/**
+		 * FileLoader instance to use during the upload.
+		 *
+		 * @member {module:upload/filerepository~FileLoader} #loader
+		 */
+		this.loader = loader;
 
-        /**
-         * The configuration of the adapter.
-         *
-         * @member {module:upload/adapters/simpleuploadadapter~SimpleUploadConfig} #options
-         */
-        this.options = options;
-    }
+		/**
+		 * The configuration of the adapter.
+		 *
+		 * @member {module:upload/adapters/simpleuploadadapter~SimpleUploadConfig} #options
+		 */
+		this.options = options;
+	}
 
-    /**
-     * Starts the upload process.
-     *
-     * @see module:upload/filerepository~UploadAdapter#upload
-     * @returns {Promise}
-     */
-    upload() {
-        return this.loader.file
-            .then( file => new Promise( ( resolve, reject ) => {
-                this._initRequest();
-                this._initListeners( resolve, reject, file );
-                this._sendRequest( file );
-            } ) );
-    }
+	/**
+	 * Starts the upload process.
+	 *
+	 * @see module:upload/filerepository~UploadAdapter#upload
+	 * @returns {Promise}
+	 */
+	upload() {
+		return this.loader.file
+			.then( file => new Promise( ( resolve, reject ) => {
+				this._initRequest();
+				this._initListeners( resolve, reject, file );
+				this._sendRequest( file );
+			} ) );
+	}
 
-    /**
-     * Aborts the upload process.
-     *
-     * @see module:upload/filerepository~UploadAdapter#abort
-     * @returns {Promise}
-     */
-    abort() {
-        if ( this.xhr ) {
-            this.xhr.abort();
-        }
-    }
+	/**
+	 * Aborts the upload process.
+	 *
+	 * @see module:upload/filerepository~UploadAdapter#abort
+	 * @returns {Promise}
+	 */
+	abort() {
+		if ( this.xhr ) {
+			this.xhr.abort();
+		}
+	}
 
-    /**
-     * Initializes the `XMLHttpRequest` object using the URL specified as
-     * {@link module:upload/adapters/simpleuploadadapter~SimpleUploadConfig#uploadUrl `simpleUpload.uploadUrl`} in the editor's
-     * configuration.
-     *
-     * @private
-     */
-    _initRequest() {
-        const xhr = this.xhr = new XMLHttpRequest();
+	/**
+	 * Initializes the `XMLHttpRequest` object using the URL specified as
+	 * {@link module:upload/adapters/simpleuploadadapter~SimpleUploadConfig#uploadUrl `simpleUpload.uploadUrl`} in the editor's
+	 * configuration.
+	 *
+	 * @private
+	 */
+	_initRequest() {
+		const xhr = this.xhr = new XMLHttpRequest();
 
-        xhr.open( 'POST', this.options.uploadUrl, true );
-        xhr.responseType = 'json';
-    }
+		xhr.open( 'POST', this.options.uploadUrl, true );
+		xhr.responseType = 'json';
+	}
 
-    /**
-     * Initializes XMLHttpRequest listeners
-     *
-     * @private
-     * @param {Function} resolve Callback function to be called when the request is successful.
-     * @param {Function} reject Callback function to be called when the request cannot be completed.
-     * @param {File} file Native File object.
-     */
-    _initListeners( resolve, reject, file ) {
-        const xhr = this.xhr;
-        const loader = this.loader;
-        const genericErrorText = `Couldn't upload file: ${ file.name }.`;
+	/**
+	 * Initializes XMLHttpRequest listeners
+	 *
+	 * @private
+	 * @param {Function} resolve Callback function to be called when the request is successful.
+	 * @param {Function} reject Callback function to be called when the request cannot be completed.
+	 * @param {File} file Native File object.
+	 */
+	_initListeners( resolve, reject, file ) {
+		const xhr = this.xhr;
+		const loader = this.loader;
+		const genericErrorText = `Couldn't upload file: ${ file.name }.`;
 
-        xhr.addEventListener( 'error', () => reject( genericErrorText ) );
-        xhr.addEventListener( 'abort', () => reject() );
-        xhr.addEventListener( 'load', () => {
-            const response = xhr.response;
+		xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+		xhr.addEventListener( 'abort', () => reject() );
+		xhr.addEventListener( 'load', () => {
+			const response = xhr.response;
 
-            if ( !response || response.error ) {
-                return reject( response && response.error && response.error.message ? response.error.message : genericErrorText );
-            }
+			if ( !response || response.error ) {
+				return reject( response && response.error && response.error.message ? response.error.message : genericErrorText );
+			}
 
-            resolve( response.url ? { default: response.url } : response.urls );
-        } );
+			resolve( response.url ? { default: response.url } : response.urls );
+		} );
 
-        // Upload progress when it is supported.
-        /* istanbul ignore else */
-        if ( xhr.upload ) {
-            xhr.upload.addEventListener( 'progress', evt => {
-                if ( evt.lengthComputable ) {
-                    loader.uploadTotal = evt.total;
-                    loader.uploaded = evt.loaded;
-                }
-            } );
-        }
-    }
+		// Upload progress when it is supported.
+		/* istanbul ignore else */
+		if ( xhr.upload ) {
+			xhr.upload.addEventListener( 'progress', evt => {
+				if ( evt.lengthComputable ) {
+					loader.uploadTotal = evt.total;
+					loader.uploaded = evt.loaded;
+				}
+			} );
+		}
+	}
 
-    /**
-     * Prepares the data and sends the request.
-     *
-     * @private
-     * @param {File} file File instance to be uploaded.
-     */
-    _sendRequest( file ) {
-        // Set headers if specified.
-        const headers = this.options.headers || {};
+	/**
+	 * Prepares the data and sends the request.
+	 *
+	 * @private
+	 * @param {File} file File instance to be uploaded.
+	 */
+	_sendRequest( file ) {
+		// Set headers if specified.
+		const headers = this.options.headers || {};
 
-        for ( const headerName of Object.keys( headers ) ) {
-            this.xhr.setRequestHeader( headerName, headers[ headerName ] );
-        }
+		// Use the withCredentials flag if specified.
+		const withCredentials = this.options.withCredentials || false;
+
+		for ( const headerName of Object.keys( headers ) ) {
+			this.xhr.setRequestHeader( headerName, headers[ headerName ] );
+		}
+
+		this.xhr.withCredentials = withCredentials;
 
         // Prepare the form data.
         const data = new FormData();
 
         data.append(this.options.hasOwnProperty('fieldName') ? this.options.fieldName : 'upload', file );
 
-        // Send the request.
-        this.xhr.send( data );
-    }
+		// Send the request.
+		this.xhr.send( data );
+	}
 }
 
 /**
