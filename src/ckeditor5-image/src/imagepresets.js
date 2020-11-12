94cdca7143edcdc872d5b4d2cb877c9ab987282d
Alexey Zubarev <zajet@yusto.ru>
2020-11-12 19:27:48 +0300

    edit simple uploader for build

--- src/ckeditor5-image/src/imagepresets.js
+++ src/ckeditor5-image/src/imagepresets.js
@@ -27,45 +27,20 @@ export default class ImagePresets extends Plugin {
 		this.bind('isEnabled').to(command);
         
 		editor.editing.downcastDispatcher.on('insert:image', ( evt, data, conversionApi ) => {
-
+            
             const simpleUploadAdapterPlugin = editor.plugins.get(SimpleUploadAdapter);
-            const selectedElement = editor.model.document.selection.getSelectedElement();
             
             simpleUploadAdapterPlugin.listenTo(simpleUploadAdapterPlugin, 'change:uuid', function(evt, propName, newValue, oldValue) {
-                _this.changeUuid();
+                
+                const viewWriter = conversionApi.writer;
+                const figure = conversionApi.mapper.toViewElement(data.item);
+                const img = figure.getChild(0);
+                
+                viewWriter.setAttribute('uuid', newValue, img);
+
+                
             });
             
         }, {priority: 'hight'});
-    }
-    
-    changeUuid() {
-        const editor = this.editor;        
-    
-        editor.model.schema.extend('image', {allowAttributes: 'uuid'});
-
-        editor.conversion.for('upcast').attributeToAttribute( {
-            view: 'uuid',
-            model: 'uuid'
-        } );
-
-        editor.conversion.for('downcast').add( dispatcher => {
-            
-            dispatcher.on( 'attribute:uuid:image', ( evt, data, conversionApi ) => {
-                if (!conversionApi.consumable.consume( data.item, evt.name ) ) {
-                    return;
-                }
-
-                const viewWriter = conversionApi.writer;
-                const figure = conversionApi.mapper.toViewElement(data.item);
-                const img = figure.getChild( 0 );
-
-                if (data.attributeNewValue !== null ) {
-                    viewWriter.setAttribute('uuid', data.attributeNewValue, img);
-                } else {
-                    viewWriter.removeAttribute('uuid', img);
-                }
-            });
-            
-        });
     }
 }
